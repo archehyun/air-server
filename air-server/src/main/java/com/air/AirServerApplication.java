e1671f3611e87d561609a815fd32ad16b2487fd1
박창현 <@DESKTOP-K240QPK>
2017-11-24 23:01:11 +0900

    home

--- air-server/src/main/java/com/air/AirServerApplication.java
+++ air-server/src/main/java/com/air/AirServerApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AirServerApplication {
 
 	public static void main(String[] args) {
+		
+		System.out.println("test");
 		SpringApplication.run(AirServerApplication.class, args);
+		
 	}
 }
